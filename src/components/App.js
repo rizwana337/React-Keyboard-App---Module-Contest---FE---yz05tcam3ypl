62a446c0a62ea2c57a9a2338d2488fd02af288b5
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-16 10:45:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,37 +5,40 @@ import axios from 'axios';
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-  const [preview,setPreview]=useState('');
-  const [quote,setQuote]=useState('');
-  const handleClick=(keyValue)=>{
-    const updatedPreview=preview+keyValue;
+  const [preview, setPreview] = useState('');
+  const [quote, setQuote] = useState('');
+
+  const handleClick = (keyValue) => {
+    const updatedPreview = preview + keyValue;
     setPreview(updatedPreview);
-    if(updatedPreview==='forty two'){
-      axios.get("https://api.quotable.io/random").then((response)=>{
+
+    if (updatedPreview === 'forty two') {
+      axios.get('https://api.quotable.io/random').then((response) => {
         setQuote(response.data.content);
       });
-    }else{
+    } else {
       setQuote('');
     }
   };
-  const renderKeys=()=>{
-    return keys.map((keyValue)=>{
-      <button key={`key-${keyValue}`} id={`key-${keyValue===''?'space':keyValue}`} onClick={()=>handleClick(keyValue)}>
-      {keyValue}
+  const renderKeys = () => {
+   
+    return keys.map((keyValue) => (
+      <button key={`key-${keyValue}`} id={`key-${keyValue === ' ' ? 'space' : keyValue}`} onClick={() => handleClick(keyValue)}>
+        {keyValue}
       </button>
-      ));
-      };
+    ));
+  };
 
   return (
     <div className="App">
       <div className="preview">{preview}</div>
-      <div>
-      {preview==='forty two'&&<div className="quote">{quote}</div>}
+      {preview === 'forty two' && <div className="quote">{quote}</div>}
       <div className="keyboard">{renderKeys()}</div>
-      
-        
     </div>
   );
+
 };
+
+
 
 export default App;
